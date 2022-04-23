8b090f9777bd4953d60df8d2277057387b459e29
Vee Uye <vee.uye@gmail.com>
2022-04-23 21:03:43 +0100

    create function to pull ISS location, create button to call and add event listener

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const app = require('./src/app');
+// import fetch from 'node-fetch';
+// const fetch = require('node-fetch');
 
-const APP_PORT = 3000;
+const findISSButton = document.querySelector('#fetchdata');
+const result = document.querySelector('#result');
+async function getData() {
+  result.innerText = 'Tracking...';
+  try {
+    const res = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+    const jsonResult = await res.json();
+    result.innerText = JSON.stringify(jsonResult, null, 2);
+  } catch (error) {
+    console.log(error);
+  }
+}
+findISSButton.addEventListener('click', getData);
 
-app.listen(APP_PORT, () => {
-  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`); // eslint-disable-line
-});
+// const buttonFetch = {
+//   document.querySelector('button').addEventListener('click', async function() {
+//   await fetch('https://api.wheretheiss.at/v1/satellites/25544')
+//     // const data = response.json()
+//     .then(res => {
+//       res.json();
+//     })
+//     .then(myContent => {
+//       document.querySelector('par').innerHTML = myContent;
+
+//       }
+
+//     )}
+
+// console.log(data);
+
+// let title = '';
+// if (typeof document !== 'undefined') {
+//   title = document.title;
+// }
+// console.log(title); // '' if in a Node.js environment
+
+// async function testFetch() {
+//   const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+//   const data = await response.json();
+//   console.log(data);
+// });
+
+// testFetch();
